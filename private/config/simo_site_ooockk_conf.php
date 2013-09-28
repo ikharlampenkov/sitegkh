3072c6df89ee41b2a52732b00727404e7b32fe75
Ivan Kharlampenkov <ivan87kharlampenkov@gmail.com>
2013-09-28 19:52:28 +0700

    Обновление файлов конфигурации

--- private/config/simo_site_ooockk_conf.php
+++ private/config/simo_site_ooockk_conf.php
@@ -7,7 +7,7 @@
  * @package simo
  */
 
-$__cfg['db.dsn'] = 'mysql://sibdnevnik_:e10adc39h@localhost:3306/sibdnevnik_reu_ckk_db';
+$__cfg['db.dsn'] = 'mysql://sibdnevnik_ckk:e10adc39h@localhost:3306/sibdnevnik_reu_ckk_db';
 $__cfg['site.main.dir'] = PRIVATE_PATH . '/../';;
 $__cfg['db.driver.path'] = $__cfg['site.main.dir'] . 'private/classes/stdclass/db_driver/';
 $__cfg['db.driver.debug'] = true;
