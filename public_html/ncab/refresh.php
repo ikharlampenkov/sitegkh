40aa7cc0d3e4e880f6e67d0d94d3fe1d9dfec3c2
Ivan Kharlampenkov <ivan87kharlampenkov@gmail.com>
2015-09-15 12:32:51 +0700

    Обновление скриптов импорта данных

--- public_html/ncab/refresh.php
+++ public_html/ncab/refresh.php
@@ -16,12 +16,15 @@ include_once PRIVATE_PATH . '/config/simo_site_lesnay_conf.php';
 
 global $__cfg;
 
+$logFile = realpath(dirname(__FILE__)) . '/phplog.log';
+
 ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
 
 ini_set('max_execution_time', '180');
 ini_set('memory_limit', '128M');
 
-echo '11111';
+error_log('Start refresh at' . date('d.m.Y H:i:s'), 3, $logFile);
 
 $o_cab_xml = new gkh_cabxml(0);
 //$o_cab_xml->refreshFile($__cfg['site.dir'] . '/ncab/ftp/import.xml', $__cfg['site.dir'] . '/ncab');
@@ -31,10 +34,14 @@ $date = date('Y-m-d', mktime(0, 0, 0, date('m'), 1, date('Y')));
 //$today = true;
 $o_meters = new gkh_meters_cab(0);
 $o_meters->exportMetersValue($__cfg['site.dir'] . '/ncab', $__cfg['site.dir'] . '/ncab/ftp/export_meters.xml', $__cfg['site.dir'] . '/ncab/ftp/Export_meters.ZIP', $date);
+error_log('Generate ' . $__cfg['site.dir'] . '/ncab/ftp/Export_meters.ZIP', 3, $logFile);
 
 if (file_exists($__cfg['site.dir'] . '/ncab/ftp/IMPORT.ZIP')) {
     $o_cab_xml->refreshFile($__cfg['site.dir'] . '/ncab/ftp/IMPORT.ZIP', $__cfg['site.dir'] . '/ncab');
+    error_log('Inport ' . $__cfg['site.dir'] . '/ncab/ftp/IMPORT.ZIP', 3, $logFile);
     unlink($__cfg['site.dir'] . '/ncab/ftp/IMPORT.ZIP');
 }
+
+error_log('End refresh at' . date('d.m.Y H:i:s'), 3, $logFile);
 
 ?>
