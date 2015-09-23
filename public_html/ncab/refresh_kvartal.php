da435f9d0cb9c38a2c66181d496da89f42e84ad8
Ivan Kharlampenkov <ivan87kharlampenkov@gmail.com>
2015-09-23 12:36:12 +0700

    Перенос строк

--- public_html/ncab/refresh_kvartal.php
+++ public_html/ncab/refresh_kvartal.php
@@ -24,7 +24,7 @@ ini_set('error_reporting', E_ALL);
 ini_set('max_execution_time', '180');
 ini_set('memory_limit', '128M');
 
-error_log('Start refresh Kvartal at ' . date('d.m.Y H:i:s'), 3, $logFile);
+error_log('Start refresh Kvartal at ' . date('d.m.Y H:i:s') . "\r\n", 3, $logFile);
 
 $o_cab_xml = new gkh_cabxml(0);
 //$o_cab_xml->refreshFile($__cfg['site.dir'] . '/ncab/ftp/import.xml', $__cfg['site.dir'] . '/ncab');
@@ -34,13 +34,13 @@ $date = date('Y-m-d', mktime(0, 0, 0, date('m'), 1, date('Y')));
 //$today = true;
 $o_meters = new gkh_meters_cab(0);
 $o_meters->exportMetersValue($__cfg['site.dir'] . '/' . $__cfg['ncab.path'], $__cfg['site.dir'] . '/' . $__cfg['ncab.path'] . '/ftp/export_meters.xml', $__cfg['site.dir'] . '/' . $__cfg['ncab.path'] . '/ftp/Export_meters.ZIP', $date);
-error_log('Generate Kvartal ' . $__cfg['site.dir'] . '/ncab/ftp/Export_meters.ZIP', 3, $logFile);
+error_log('Generate Kvartal ' . $__cfg['site.dir'] . '/ncab/ftp/Export_meters.ZIP' . "\r\n", 3, $logFile);
 
 if (file_exists($__cfg['site.dir'] . '/' . $__cfg['ncab.path'] . '/ftp/IMPORT.ZIP')) {
     $o_cab_xml->refreshFile($__cfg['site.dir'] . '/' . $__cfg['ncab.path'] . '/ftp/IMPORT.ZIP', $__cfg['site.dir'] . '/' . $__cfg['ncab.path'] . '');
-    error_log('Inport Kvartal ' . $__cfg['site.dir'] . '/ncab/ftp/IMPORT.ZIP', 3, $logFile);
+    error_log('Inport Kvartal ' . $__cfg['site.dir'] . '/ncab/ftp/IMPORT.ZIP' . "\r\n", 3, $logFile);
     unlink($__cfg['site.dir'] . '/' . $__cfg['ncab.path'] . '/ftp/IMPORT.ZIP');
 }
 
-error_log('End refresh Kvartal at' . date('d.m.Y H:i:s'), 3, $logFile);
+error_log('End refresh Kvartal at' . date('d.m.Y H:i:s') . "\r\n", 3, $logFile);
 exit;
